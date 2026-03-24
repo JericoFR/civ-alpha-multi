46b25d56c9b8b11e13075efa6d073a1c4702b7a5
JericoFR <j.cancian@outlook.fr>
2026-03-24 17:40:02 +0100

    uptade socket

--- src/network/socket.js
+++ src/network/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+// ⚠️ IMPORTANT : URL serveur Render (version online)
+const SOCKET_URL = "https://civ-alpha-multi.onrender.com";
+
+export const socket = io(SOCKET_URL, {
   autoConnect: false,
 });
